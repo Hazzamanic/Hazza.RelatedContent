fe8bd1334e4c32099d48f31b833e99b988bb8dd4
Hazza <harry@modstreaming.com>
2016-12-08 11:27:20 +0000

    moved checkout logic into service added gift and order activities fixed test payment provider added more shopping tokens added query string to versioned resources map location part for displaying blog posts on a map updated related content service to accept more options start moviefanatic theme work on travail blog theme

---
+++ Models/RelatedContentContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Hazza.RelatedContent.Models {
+    public class RelatedContentContext {
+        public string[] FieldNames { get; set; } = new string[0];
+        public string ContentType { get; set; }
+        public int Count { get; set; } = 10;
+    }
+}
